6aac453b7c40f120f52f019c36bcee2cc41714ce
Paul Davis <p.davis@criteo.com>
2015-07-03 17:19:22 +0200

    Add CRTOEventQueueItem tests.
    Change-Id: Icd0a659aed87812c8d397ade58462f4ab9f0e0f4
    JIRA: MOB-439

--- advertiser-sdk/advertiser-sdkTests/network/CRTOEventQueueItemTests.m
+++ advertiser-sdk/advertiser-sdkTests/network/CRTOEventQueueItemTests.m
@@ -8,32 +8,66 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "CRTOEventQueueItem.h"
+#import "CRTOEvent.h"
+#import "CRTOEvent+Internal.h"
+#import "CRTOHomeViewEvent.h"
+
 @interface CRTOEventQueueItemTests : XCTestCase
 
 @end
 
 @implementation CRTOEventQueueItemTests
-
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+{
+    NSString* requestBodyString;
+    NSData* requestBodyData;
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)setUp
+{
+    [super setUp];
+
+    requestBodyString = @"some string";
+
+    requestBodyData = [NSData dataWithBytes:requestBodyString.UTF8String
+                                     length:[requestBodyString lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
+}
+
+- (void)tearDown
+{
+
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void) testInit
+{
+    CRTOEventQueueItem* item = [[CRTOEventQueueItem alloc] init];
+
+    XCTAssertNotNil(item);
+
+    XCTAssertGreaterThan(item.age, 86400 * 365 * 10);
+
+    XCTAssertNil(item.event);
+    XCTAssertNil(item.requestBody);
+
+    XCTAssertNotNil(item.responseData);
+    XCTAssertEqual(item.responseData.length, 0);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void) testInitEventRequestBody
+{
+    CRTOHomeViewEvent* homeEvent = [[CRTOHomeViewEvent alloc] init];
+    homeEvent.timestamp = [NSDate date];
+
+    CRTOEventQueueItem* item = [[CRTOEventQueueItem alloc] initWithEvent:homeEvent
+                                                             requestBody:requestBodyString];
+
+    XCTAssertLessThan(item.age, 10);
+    XCTAssertEqual(item.event, homeEvent);
+    XCTAssertEqualObjects(item.requestBody, requestBodyData);
+
+    XCTAssertNotNil(item.responseData);
+    XCTAssertEqual(item.responseData.length, 0);
 }
 
 @end
