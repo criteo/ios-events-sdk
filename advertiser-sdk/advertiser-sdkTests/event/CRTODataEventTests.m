409c48f77081fe09069b1e046156083660a2e1df
Paul Davis <p.davis@criteo.com>
2015-07-06 14:16:30 +0200

    Adds CRTODataEvent tests.
    Includes fun extra data testing.
    
    Change-Id: I9b3c3a56dc8c7c75d922bdbd97d397b21bd3e6c2
    JIRA: MOB-439

--- advertiser-sdk/advertiser-sdkTests/event/CRTODataEventTests.m
+++ advertiser-sdk/advertiser-sdkTests/event/CRTODataEventTests.m
@@ -8,32 +8,116 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "CRTODataEvent.h"
+
 @interface CRTODataEventTests : XCTestCase
 
 @end
 
 @implementation CRTODataEventTests
+{
+    CRTODataEvent* dataEvent;
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    NSMutableString* dateKey;
+    NSMutableString* integerKey;
+    NSMutableString* floatKey;
+    NSMutableString* stringKey;
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)setUp
+{
+    [super setUp];
+
+    dataEvent = [[CRTODataEvent alloc] init];
+
+    dateKey    = [NSMutableString stringWithString:@"A_Date_Key"];
+    integerKey = [NSMutableString stringWithString:@"An_Integer_Key"];
+    floatKey   = [NSMutableString stringWithString:@"A_Float_Key"];
+    stringKey  = [NSMutableString stringWithString:@"A_String_Key"];
+}
+
+- (void)tearDown
+{
+
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void) testInit
+{
+    XCTAssertNotNil(dataEvent);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void) testDateExtraData
+{
+    NSDate* someDate = [NSDate dateWithTimeIntervalSince1970:1000000000];
+
+    [dataEvent setDateExtraData:someDate ForKey:dateKey];
+
+    NSDate* resultDate = [dataEvent dateExtraDataForKey:@"A_Date_Key"];
+
+    XCTAssertEqualObjects(someDate, resultDate);
 }
+
+- (void) testDateExtraDataNoValue
+{
+    NSDate* resultDate = [dataEvent dateExtraDataForKey:@"BadKey"];
+
+    XCTAssertNil(resultDate);
+}
+
+- (void) testIntegerExtraData
+{
+    NSInteger someInteger = 0xdeadbeef;
+
+    [dataEvent setIntegerExtraData:someInteger ForKey:integerKey];
+
+    NSInteger resultInteger = [dataEvent integerExtraDataForKey:@"An_Integer_Key"];
+
+    XCTAssertEqual(someInteger, resultInteger);
+}
+
+- (void) testIntegerExtraDataNoValue
+{
+    NSInteger resultInteger = [dataEvent integerExtraDataForKey:@"BadKey"];
+
+    XCTAssertEqual(resultInteger, 0);
+}
+
+- (void) testFloatExtraData
+{
+    float someFloat = 999.85;
+
+    [dataEvent setFloatExtraData:someFloat ForKey:floatKey];
+
+    float resultFloat = [dataEvent floatExtraDataForKey:@"A_Float_Key"];
+
+    XCTAssertEqual(someFloat, resultFloat);
+}
+
+- (void) testFloatExtraDataNoValue
+{
+    float resultFloat = [dataEvent floatExtraDataForKey:@"BadKey"];
+
+    XCTAssertEqual(resultFloat, 0.0f);
+}
+
+- (void) testStringExtraData
+{
+    NSString* someString = @"Four score and seven years ago...";
+
+    [dataEvent setStringExtraData:someString ForKey:stringKey];
+
+    NSString* resultString = [dataEvent stringExtraDataForKey:@"A_String_Key"];
+
+    XCTAssertEqualObjects(someString, resultString);
+}
+
+- (void) testStringExtraDataNoValue
+{
+    NSString* resultString = [dataEvent stringExtraDataForKey:@"BadKey"];
+
+    XCTAssertNil(resultString);
+}
+
 
 @end
