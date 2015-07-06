8080e4d123b84e6e3c4ee24c1c4bf7388ba3c4a1
Paul Davis <p.davis@criteo.com>
2015-07-06 17:28:45 +0200

    Adds transaction confirmation event tests.
    Change-Id: I0cd4281235796add2a096df86fd7260e29a2189e
    JIRA: MOB-439

--- advertiser-sdk/advertiser-sdkTests/event/CRTOTransactionConfirmationEventTests.m
+++ advertiser-sdk/advertiser-sdkTests/event/CRTOTransactionConfirmationEventTests.m
@@ -8,32 +8,184 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "CRTOBasketProduct.h"
+#import "CRTOTransactionConfirmationEvent.h"
+
 @interface CRTOTransactionConfirmationEventTests : XCTestCase
 
 @end
 
 @implementation CRTOTransactionConfirmationEventTests
+{
+    NSString* productId1;
+    NSString* productId2;
+    NSString* productId3;
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    double price1;
+    double price2;
+    double price3;
+
+    NSInteger quantity1;
+    NSInteger quantity2;
+    NSInteger quantity3;
+
+    NSString* currency;
+
+    NSString* transactionId;
+
+    NSDate* startDate;
+    NSDate* endDate;
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)setUp
+{
+    [super setUp];
+
+    productId1 = @"foo";
+    productId2 = @"bar";
+    productId3 = @"나는 유리를 먹을 수 있어요. 그래도 아프지 않아요";
+
+    price1     = 999.85;
+    price2     = 0.1;
+    price3     = 0.1 + 0.1 + 0.1;
+
+    quantity1  = 1;
+    quantity2  = 10;
+    quantity3  = -100;
+
+    currency  = @"CAN";
+
+    transactionId = @"TXID1234567890987654321";
+
+    startDate = [NSDate dateWithTimeIntervalSince1970:1000000000];
+    endDate   = [NSDate dateWithTimeIntervalSince1970:1000086401];
+}
+
+- (void)tearDown
+{
+
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void) testInit
+{
+    CRTOTransactionConfirmationEvent* event = [[CRTOTransactionConfirmationEvent alloc] init];
+
+    XCTAssertNotNil(event);
+
+    XCTAssertNil(event.currency);
+    XCTAssertNil(event.basketProducts);
+    XCTAssertNil(event.transactionId);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void) testInitWithBasketProducts
+{
+    NSMutableArray* products_init = [NSMutableArray arrayWithArray:@[ [[CRTOBasketProduct alloc] initWithProductId:productId1 price:price1 quantity:quantity1],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId2 price:price2 quantity:quantity2],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId3 price:price3 quantity:quantity3] ]];
+
+    CRTOTransactionConfirmationEvent* event = [[CRTOTransactionConfirmationEvent alloc] initWithBasketProducts:products_init];
+
+    XCTAssertNotNil(event);
+
+    XCTAssertNil(event.currency);
+
+    XCTAssertNotNil(event.basketProducts);
+    XCTAssertEqual(event.basketProducts.count, 3);
+    XCTAssertNotEqual(event.basketProducts, products_init);
+    XCTAssertEqualObjects(event.basketProducts, products_init);
+
+    XCTAssertNil(event.transactionId);
+}
+
+- (void) testInitWithBasketProductsTransactionId
+{
+    NSMutableArray* products_init = [NSMutableArray arrayWithArray:@[ [[CRTOBasketProduct alloc] initWithProductId:productId1 price:price1 quantity:quantity1],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId2 price:price2 quantity:quantity2],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId3 price:price3 quantity:quantity3] ]];
+
+    CRTOTransactionConfirmationEvent* event = [[CRTOTransactionConfirmationEvent alloc] initWithBasketProducts:products_init transactionId:transactionId];
+
+    XCTAssertNotNil(event);
+
+    XCTAssertNil(event.currency);
+
+    XCTAssertNotNil(event.basketProducts);
+    XCTAssertEqual(event.basketProducts.count, 3);
+    XCTAssertNotEqual(event.basketProducts, products_init);
+    XCTAssertEqualObjects(event.basketProducts, products_init);
+
+    XCTAssertEqualObjects(event.transactionId, transactionId);
+}
+
+- (void) testInitWithBasketProductsTransactionIdCurrency
+{
+    NSMutableArray* products_init = [NSMutableArray arrayWithArray:@[ [[CRTOBasketProduct alloc] initWithProductId:productId1 price:price1 quantity:quantity1],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId2 price:price2 quantity:quantity2],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId3 price:price3 quantity:quantity3] ]];
+
+    CRTOTransactionConfirmationEvent* event = [[CRTOTransactionConfirmationEvent alloc] initWithBasketProducts:products_init
+                                                                                                 transactionId:transactionId
+                                                                                                      currency:currency];
+
+    XCTAssertNotNil(event);
+
+    XCTAssertEqualObjects(event.currency, currency);
+
+    XCTAssertNotNil(event.basketProducts);
+    XCTAssertEqual(event.basketProducts.count, 3);
+    XCTAssertNotEqual(event.basketProducts, products_init);
+    XCTAssertEqualObjects(event.basketProducts, products_init);
+
+    XCTAssertEqualObjects(event.transactionId, transactionId);
+}
+
+- (void) testInitWithBasketProductsTransactionIdCurrencyDates
+{
+    NSMutableArray* products_init = [NSMutableArray arrayWithArray:@[ [[CRTOBasketProduct alloc] initWithProductId:productId1 price:price1 quantity:quantity1],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId2 price:price2 quantity:quantity2],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId3 price:price3 quantity:quantity3] ]];
+
+    CRTOTransactionConfirmationEvent* event = [[CRTOTransactionConfirmationEvent alloc] initWithBasketProducts:products_init
+                                                                                                 transactionId:transactionId
+                                                                                                      currency:currency
+                                                                                                     startDate:startDate
+                                                                                                       endDate:endDate];
+
+    XCTAssertNotNil(event);
+
+    XCTAssertEqualObjects(event.currency, currency);
+
+    XCTAssertNotNil(event.basketProducts);
+    XCTAssertEqual(event.basketProducts.count, 3);
+    XCTAssertNotEqual(event.basketProducts, products_init);
+    XCTAssertEqualObjects(event.basketProducts, products_init);
+
+    XCTAssertEqualObjects(event.transactionId, transactionId);
+
+    XCTAssertEqualObjects(event.startDate, startDate);
+    XCTAssertEqualObjects(event.endDate, endDate);
+}
+
+- (void) testTransactionConfirmationEventCopy
+{
+    NSMutableArray* products_init = [NSMutableArray arrayWithArray:@[ [[CRTOBasketProduct alloc] initWithProductId:productId1 price:price1 quantity:quantity1],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId2 price:price2 quantity:quantity2],
+                                                                      [[CRTOBasketProduct alloc] initWithProductId:productId3 price:price3 quantity:quantity3] ]];
+
+    CRTOTransactionConfirmationEvent* event = [[CRTOTransactionConfirmationEvent alloc] initWithBasketProducts:products_init
+                                                                                                 transactionId:transactionId
+                                                                                                      currency:currency
+                                                                                                     startDate:startDate
+                                                                                                       endDate:endDate];
+
+    CRTOTransactionConfirmationEvent* eventCopy = [event copy];
+
+    XCTAssertEqualObjects(event.basketProducts, eventCopy.basketProducts);
+    XCTAssertEqualObjects(event.currency, eventCopy.currency);
+    XCTAssertEqualObjects(event.transactionId, transactionId);
+    XCTAssertEqualObjects(event.startDate, eventCopy.startDate);
+    XCTAssertEqualObjects(event.endDate, eventCopy.endDate);
 }
 
 @end
